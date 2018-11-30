705d12f4b6bf49a853716cc0adc9a52616445c4c
Jianing Yu <yujianing001@qq.com>
2018-11-29 22:53:45 -0500

    fix undefined typeand change to post method

--- models/wallpaper.js
+++ models/wallpaper.js
@@ -3,7 +3,14 @@ var wallpaperSchema = new mongoose.Schema({
     //_id:String,
     _id: mongoose.Schema.Types.ObjectId,
     url: String,
-    likes: Number
+    likes: {
+        type:Number,
+        default: 0
+    },
+    collects: {
+        type:Number,
+        default: 0
+    }
 });
 
 module.exports = mongoose.model('wallpaper', wallpaperSchema);
